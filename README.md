ca5b3f3e3d071b8aafdbfcfca8c0b3e847f32bc6
Utero93 <nirvanautero93@gmail.com>
2023-12-07 13:58:10 -0500

    installed the node package manager @ version 8.2.4

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Readme-Gen
-A high-quality README for any app
+# blah
+
+  ## License
+
+This project is licensed under the [adad License] - see LICENSE file for more details.
+
+  
+
+  ## Description
+
+  maflf;malfm
+
+  ## Table of Contents 
+  - [License](#license)
+  -[Description](#description)
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[Constributing](#contributing)
+  -[Tests](#tests)
+  -[Questions](#questions)
+  
+  ## Installation
+
+  afad
+
+  ## Usage
+
+  visit: adad
+
+  adad
+
+  ## Contributing 
+
+  Thank you for your contribution to this project! I appreciate any and all future additions. 
+
+  ## How to Contribute to the App 
+
+  undefined
+
+  ## Tests 
+
+  This project includes a small series of tests to maintain its currents functionality. Before the tests run, check that you have installed all the necessary dependencies.
+
+  In order to test this project, follow these steps listed below: 
+
+  adad
+
+  ## Questions 
+
+  [Find me on Github!](github.com/adad)
+
+If you have any further questions, feel free to reach me via adad
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -57,11 +57,61 @@ function renderLicenseToC(license){
   }
 };
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseSection(data.license)}
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents 
+  ${renderLicenseToC(data.license)}
+  -[Description](#description)
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[Constributing](#contributing)
+  -[Tests](#tests)
+  -[Questions](#questions)
+  
+  ## Installation
+
+  ${data.install}
+
+  ## Usage
+
+  visit: ${data.link}
+
+  ${data.usage}
+
+  ## Contributing 
+
+  Thank you for your contribution to this project! I appreciate any and all future additions. 
+
+  ## How to Contribute to the App 
+
+  ${data.contribute}
+
+  ## Tests 
+
+  This project includes a small series of tests to maintain its currents functionality. Before the tests run, check that you have installed all the necessary dependencies.
+
+  In order to test this project, follow these steps listed below: 
+
+  ${data.tests}
+
+  ## Questions 
+
+  [Find me on Github!](github.com/${data.username})
+
+If you have any further questions, feel free to reach me via ${data.email}
 
 `;
 }
 
+// export the module 
 module.exports = generateMarkdown;
