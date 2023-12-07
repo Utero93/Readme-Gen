c93345596fc28f54038e13acaf383cc0fa51bf59
Utero93 <nirvanautero93@gmail.com>
2023-12-07 13:18:36 -0500

    ceeating the functions for the licences in the generate markdown js file

--- index.js
+++ index.js
@@ -27,57 +27,57 @@ const questions = [
     // Installation Instructions
     {
         type: 'input',
-        name: 'title',
-        message: 'What is the title of your application?', 
+        name: 'install',
+        message: 'Write a detailed guide of how to install your application.', 
     }, 
 
     // Deployed Project Link
     {
         type: 'input',
-        name: 'title',
-        message: 'What is the title of your application?', 
+        name: 'link',
+        message: 'What is the url link for your deployed application?', 
     }, 
     // Usage Instructions 
     {
         type: 'input',
-        name: 'title',
-        message: 'What is the title of your application?', 
+        name: 'usage',
+        message: 'Write a description informing users how your application runs.', 
     }, 
 
     // License 
     {
         type: 'input',
-        name: 'title',
-        message: 'What is the title of your application?', 
+        name: 'license',
+        message: 'Which license covers your application?', 
         choices: ['MIT', 'Apache 2.0', 'GPL-3-0', 'BSD 3', 'Creative Commons', 'Unlicensed']
     }, 
 
     // Contributors 
     {
         type: 'input',
-        name: 'title',
-        message: 'What is the title of your application?', 
+        name: 'constribute',
+        message: 'In detail, add guidelines and accurate information for users when contributing to your work.', 
     }, 
 
     // Tests
     {
         type: 'input',
-        name: 'title',
-        message: 'What is the title of your application?', 
+        name: 'tests',
+        message: 'Write a detailed guide informing users on how to properly test this application.',  
     }, 
 
     // Github username input 
     {
         type: 'input',
-        name: 'title',
-        message: 'What is the title of your application?', 
+        name: 'username',
+        message: 'What is the username for your github profile?', 
     }, 
 
     // Email input 
     {
         type: 'input',
-        name: 'title',
-        message: 'What is the title of your application?', 
+        name: 'email',
+        message: 'Enter a preferred email for users to contact you with any questions, comments, and concerns.',
     }, 
 
 ];
